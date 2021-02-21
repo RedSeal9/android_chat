2f32fa2f1083855c0c471e8fce429b8f632df4da
RedSeal9 <30299681+RedSeal9@users.noreply.github.com>
2021-02-21 16:57:05 -0500

    testing volley

--- app/src/main/java/com/redseal/chat/MainActivity.kt
+++ app/src/main/java/com/redseal/chat/MainActivity.kt
@@ -1,11 +1,38 @@
 package com.redseal.chat
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
 
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val textView = findViewById<TextView>(R.id.text)
+// ...
+
+// Instantiate the RequestQueue.
+        val queue = Volley.newRequestQueue(this)
+        val url = "https://max3.app"
+
+// Request a string response from the provided URL.
+        val stringRequest = StringRequest(Request.Method.GET, url,
+                { response ->
+                    // Display the first 500 characters of the response string.
+                    textView.text = response
+                },
+                { textView.text = "something borked for some reason\n\ncheck if you have an internet connection\n\n-redseal" })
+
+// Add the request to the RequestQueue.
+        queue.add(stringRequest)
+
+
     }}
